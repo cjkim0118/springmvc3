0deb4db9fa85efc887958f6b5efa6b23d511ddd0
cjkim0118 <cjkim0118@gmail.com>
2020-08-09 18:33:56 +0900

    Chapter 10 - CRUD 및 JUnit Test

--- src/test/java/org/zerock/service/BoardServiceTests.java
+++ src/test/java/org/zerock/service/BoardServiceTests.java
@@ -38,10 +38,10 @@ public class BoardServiceTests {
 //		service.register(board);
 //		log.info("생성된 게시물의 번호: " +board.getBno());
 //	}
-//	@Test
-//	public void testGetList() {
-//		service.getList().forEach(board -> log.info(board));
-//	}
+	@Test
+	public void testGetList() {
+		service.getList().forEach(board -> log.info(board));
+	}
 //	@Test
 //	public void testGet() {
 //		log.info(service.get(1L));
@@ -60,9 +60,9 @@ public class BoardServiceTests {
 //		log.info("MODIFY RESULT: " + board);
 //		
 //	}
-	
-	@Test
-	public void testDelete() {
-		log.info("REMOVE RESULT: " +service.remove(2L));
-	}
+//	
+//	@Test
+//	public void testDelete() {
+//		log.info("REMOVE RESULT: " +service.remove(2L));
+//	}
 }
